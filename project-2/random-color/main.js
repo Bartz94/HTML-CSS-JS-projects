bc0f9aa300e6fcaf8b0ab879351873f9f8939675
Bartosz Czyzewski <bart94946@gmail.com>
2021-10-17 19:05:21 +0200

    Added second button for rgb, added styles for buttons and h1

--- project-2/random-color/main.js
+++ project-2/random-color/main.js
@@ -1,13 +1,21 @@
 function init() {
-    const rndColorBtn = document.querySelector('.color_button');
+    const hexColorButton = document.querySelector('.hex_color_button');
+    const rgbColorButton = document.querySelector('.rgb_color_button');
     const body = document.querySelector('body');
     const colorName = document.querySelector('.color_name')
 
-    rndColorBtn.addEventListener('click', () => {
+    hexColorButton.addEventListener('click', () => {
         const randomColor = '#' + Math.random().toString(16).slice(2, 8);
         body.style.backgroundColor = randomColor;
         console.log(randomColor)
-        colorName.innerText = randomColor
+        colorName.innerText = 'Hex Code: ' + randomColor
+    })
+    rgbColorButton.addEventListener('click', () => {
+        const randomColor = '#' + Math.random().toString(16).slice(2, 8);
+        body.style.backgroundColor = randomColor;
+        console.log(randomColor)
+        // colorName.innerText = 'Hex Code: ' + randomColor
+        colorName.innerText = 'RGB Code: ' + body.style.backgroundColor
     })
 };
 
