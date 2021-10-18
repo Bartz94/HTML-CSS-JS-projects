f706f142bb8fdcb4f3c840760b91274efc76fce3
Bartosz Czyzewski <bart94946@gmail.com>
2021-10-18 13:17:00 +0200

    Added button for copying color code

--- project-2/random-color/main.js
+++ project-2/random-color/main.js
@@ -1,22 +1,30 @@
 function init() {
     const hexColorButton = document.querySelector('.hex_color_button');
     const rgbColorButton = document.querySelector('.rgb_color_button');
+    copyButton = document.querySelector('.copy_button')
     const body = document.querySelector('body');
     const colorName = document.querySelector('.color_name')
 
     hexColorButton.addEventListener('click', () => {
         const randomColor = '#' + Math.random().toString(16).slice(2, 8);
         body.style.backgroundColor = randomColor;
-        console.log(randomColor)
-        colorName.innerText = 'Hex Code: ' + randomColor
+        colorName.innerText = 'Code: ' + randomColor
     })
     rgbColorButton.addEventListener('click', () => {
         const randomColor = '#' + Math.random().toString(16).slice(2, 8);
         body.style.backgroundColor = randomColor;
-        console.log(randomColor)
-        // colorName.innerText = 'Hex Code: ' + randomColor
-        colorName.innerText = 'RGB Code: ' + body.style.backgroundColor
+        colorName.innerText = 'Code: ' + body.style.backgroundColor
     })
+
+    copyButton.addEventListener('click', function copy() {
+        var temp = document.createElement("textarea");
+        document.body.appendChild(temp);
+        temp.value = colorName.innerText
+        temp.select();
+        document.execCommand("copy");
+        document.body.removeChild(temp);
+    })
+
 };
 
 window.onload = init;
