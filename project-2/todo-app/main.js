7be4daf7786b760777a8e1f6372d5a6b49e0d486
Bartosz Czyzewski <bart94946@gmail.com>
2021-11-09 19:34:26 +0100

    Added remembering tasks after refresh of a page

--- project-2/todo-app/main.js
+++ project-2/todo-app/main.js
@@ -3,35 +3,56 @@ function init() {
     const todoInput = document.querySelector('.todo_input');
     const todoList = document.querySelector('.todo_list');
 
+    if (localStoragedTasks) {
+        localStoragedTasks.forEach(task => {
+            createNewItem(task);
+        });
+    };
+
     todoInput.focus();
 
-    todoForm.addEventListener('submit', createNewItem)
-
-    function createNewItem(event) {
+    todoForm.addEventListener('submit', (event) => {
         event.preventDefault();
+        createNewItem();
+    });
 
-        const newItem = document.createElement('li');
-        newItem.innerText = todoInput.value;
+    function createNewItem(task) {
+        let taskText = todoInput.value;
 
-        newItem.addEventListener('click', () => {
-            newItem.classList.toggle('completed')
+        if (task) {
+            taskText = task.text;
+        }
+
+        if (taskText) {
+            const newItem = document.createElement('li');
+            if (task && task.completed) {
+                newItem.classList.add('completed');
+            };
+
+            newItem.innerText = taskText;
+
+            newItem.addEventListener('click', () => {
+                newItem.classList.toggle('completed')
+                updateLocalStorage();
+            });
+
+            newItem.addEventListener('contextmenu', (event) => {
+                event.preventDefault();
+                newItem.remove();
+                updateLocalStorage();
+            });
+
+            todoList.appendChild(newItem);
+            todoInput.value = '';
             updateLocalStorage();
-        });
+        };
 
-        newItem.addEventListener('contextmenu', (event) => {
-            event.preventDefault();
-            newItem.remove();
-        });
-
-        todoList.appendChild(newItem);
-        todoInput.value = '';
-        updateLocalStorage();
     };
 };
 
 function updateLocalStorage() {
-    const taskElements = document.querySelectorAll('li');
     const arrayOfTask = [];
+    const taskElements = document.querySelectorAll('li');
 
     taskElements.forEach((taskElement) => {
         arrayOfTask.push({
@@ -40,7 +61,6 @@ function updateLocalStorage() {
         });
     });
 
-    localStorage.setItem('Tasks', JSON.stringify(arrayOfTask));
-    console.log(arrayOfTask)
+    localStorage.setItem('tasks', JSON.stringify(arrayOfTask));
 };
 window.onload = init;
