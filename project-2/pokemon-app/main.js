8b934b72e4778e69c053a25148720648e1395965
Bartosz Czyzewski <bart94946@gmail.com>
2021-11-14 21:37:54 +0100

    Added function createPokemonBox

--- project-2/pokemon-app/main.js
+++ project-2/pokemon-app/main.js
@@ -7,13 +7,24 @@ const getMaxPokemons = async () => {
     }
 }
 
-
 const getPokemon = async id => {
     const link = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(link);
     const pokemon = await res.json();
-    console.log(pokemon.name)
+    createPokemonBox(pokemon)
 }
 
+function createPokemonBox(pokemon) {
+    const pokemonBox = document.createElement('div');
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);;
+
+
+    const pokeBoxInnerHTML = `
+    ${name}
+    `;
+
+    pokemonBox.innerHTML = pokeBoxInnerHTML;
+    pokeBoxContainer.appendChild(pokemonBox)
+}
 
 getMaxPokemons();
