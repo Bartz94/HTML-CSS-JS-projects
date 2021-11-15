8adefa2319b572c38313225dde45563bb05f79a4
Bartosz Czyzewski <bart94946@gmail.com>
2021-11-15 18:46:16 +0100

    Added backgroundcolor equal to pokemon type

--- project-2/pokemon-app/main.js
+++ project-2/pokemon-app/main.js
@@ -1,6 +1,31 @@
 const maxPokemons = 151;
 const pokeBoxContainer = document.querySelector('#container');
 
+const typeColors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+};
+
+const renderColors = Object.keys(typeColors)
+
+console.log(renderColors)
+
 const getMaxPokemons = async () => {
     for (let i = 1; i <= maxPokemons; i++) {
         await getPokemon(i);
@@ -12,21 +37,26 @@ const getPokemon = async id => {
     const res = await fetch(link);
     const pokemon = await res.json();
     createPokemonBox(pokemon)
-    console.log(pokemon)
+    // console.log(pokemon.types[0].type.name)
 }
 
 function createPokemonBox(pokemon) {
     const pokemonBox = document.createElement('div');
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);;
+    const typeName = pokemon.types[0].type.name[0].toUpperCase() + pokemon.types[0].type.name.slice(1);
 
+    const themeColor = typeColors[pokemon.types[0].type.name];
+    console.log(themeColor)
 
     const pokeBoxInnerHTML = `
-    <p class="poke_name">#${pokemon.id} ${name}</p>
+    <p class="poke_name">#${pokemon.id.toString().padStart(3, '0')} ${name}</p>
      <img class ="pokemon_image" src="${pokemon.sprites.other["official-artwork"].front_default}" width="200" height="200">
+    <p class="pokemon_type">${typeName}</p>
     `;
 
     pokemonBox.innerHTML = pokeBoxInnerHTML;
-    pokemonBox.classList.add('poke_card')
+    pokemonBox.classList.add('poke_card');
+    pokemonBox.style.backgroundColor = themeColor;
     pokeBoxContainer.appendChild(pokemonBox)
 }
 
